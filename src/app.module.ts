660fdd5559dd24b962e988fa5c1c044a4e3f5ea6
AndreaQuelali <andrealizbethquelali5@gmail.com>
2025-10-08 19:32:45 -0400

    feat: update module app and add recently service with method add and listSongs

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { AuthModule } from './auth/auth.module';
 import { MixesModule } from './mixes/mixes.module';
 import { FavoritesModule } from './favorites/favorites.module';
 import { ConfigModule } from '@nestjs/config';
+import { RecentlyModule } from './recently/recently.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, SongsModule, AuthModule, MixesModule,FavoritesModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, SongsModule, AuthModule, MixesModule, FavoritesModule, RecentlyModule],
   controllers: [AppController],
   providers: [AppService],
 })
